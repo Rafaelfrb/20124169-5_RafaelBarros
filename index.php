f6d87879faa4da7b275d525ed540cd71e45a3fa1
thamiris.barros <rafael.frb@hotmail.com>
2021-08-27 20:06:52 -0300

    Tentativa site

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brasilia Sweets - Docinhos caseiros</title>
+    <style>
+        body {
+            margin: 0;
+        }
+        header {
+            height: 187px;
+            background-color: #DD6A00;
+            text-align: center;
+            padding: 10px;
+            
+        }
+        .logo {
+            height: 167px;
+        }
+    </style>
+</head>
+<body>
+    <header><img src="images/logo.png" alt="Logo" class="logo"></header>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Quem somos</li>
+            <li>Contato</li>
+            <li>Endereço</li>
+        </ul>
+    </nav>
+    <div>conteúdo</div>
+    <footer>Footer</footer> 
+</body>
+</html>
