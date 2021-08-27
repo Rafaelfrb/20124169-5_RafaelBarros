6b9adf35d6540d5b4e0fe11a5b0e6526bcdfd718
thamiris.barros <rafael.frb@hotmail.com>
2021-08-27 20:25:12 -0300

    Header e footer

--- index.php
+++ index.php
@@ -5,9 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brasilia Sweets - Docinhos caseiros</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@300;400;700&display=swap" rel="stylesheet">
     <style>
         body {
             margin: 0;
+            font-family: 'Bitter', serif;
         }
         header {
             height: 187px;
@@ -18,6 +22,35 @@
         }
         .logo {
             height: 167px;
+        }
+        nav {
+           background-color: #FFAD93; 
+           margin: 0;
+
+        }
+        nav ul{
+            list-style: none;
+            width: 100%;
+            margin: 0px;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+        }
+        nav ul li {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            padding-left: 25px;
+            padding-right: 25px;
+
+        }
+        .content {
+            min-height: 400px;
+        
+        }
+        footer {
+            background-color: #FFAD93;
+            min-height: 60px;
+            padding: 40px;
         }
     </style>
 </head>
@@ -31,7 +64,7 @@
             <li>Endereço</li>
         </ul>
     </nav>
-    <div>conteúdo</div>
-    <footer>Footer</footer> 
+    <div class="content">conteúdo</div>
+    <footer>Rafael do N. F. Barros RA: 20124169-5</footer> 
 </body>
 </html>
